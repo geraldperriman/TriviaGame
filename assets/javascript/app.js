ce0a05edab31ce071fd364caa8ae3ac5a3042397
Gerald Perriman <geraldperriman@Geralds-MacBook-Pro.local>
2017-05-29 17:10:54 -0400

    questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,102 @@
+
+
+var triviaQuestions = [{
+	question: " Martin had a crush on a school teacher when he was younger. What was the name of the teacher?",
+	answerList: ["Ms. Jackson", "Ms. Trindad", "Ms. Thomas", "Ms.Tobago"],
+	answer: 1
+},{
+	question: "When Martin got fired, he picked up a plastic garbage can and threw it against the window screaming what?",
+	answerList: ["Radio", "Stereo!", "Noooo!", "Damn"],
+	answer: 0
+},{
+	question: "Martin went on a rent strike because his rent was increased and the landlord refused to fix any of the appliances around the apartment. How much was the increase?",
+	answerList: ["5.00", "50.00", "500.00", "250.00"],
+	answer: 0
+},{
+	question: "Pam had a boyfriend that had his own skybox in Pistons' stadium. What was his name?",
+	answerList: ["Isiah", "Joe", "Simon", "Bill"],
+	answer: 2
+},{
+	question: "Martin received a $4,000 dollar investment and wanted to build a restaurant with the money. What did he call the restaurant?",
+	answerList: ["Martin's Place", "Wazzup!", "Rent Em Spoons", "Marty-Mart's Meatloaf and Waffles"],
+	answer: 3
+},{
+	question: " What was Gina's boss name??",
+	answerList: ["Mr. Walter", "Mr.Walker", "Mr.Whitaker", "Mr.Williams"],
+	answer: 2
+},{
+	question: "Who was Martin's nemesis in high school?",
+	answerList: ["Pretty Larry", "Pretty Rickey", "Pretty Boy", "Mike-Mike"],
+	answer: 1
+},{
+	question: "Who killed Mama Payne's bird?",
+	answerList: ["Pam", "Martin", "Gina", "Tommy"],
+	answer: 2
+},{
+	question: "What are the name's of Shanehneh's two best friends?",
+	answerList: ["Laquisha & Renee", "Laquita & Keylolo", "Trina & Katina", "Keisha & Lakreshia"],
+	answer: 1
+},{
+	question: "What are the names of Martin and Gina's exes that they invite to dinner?",
+	answerList: ["Joyce & Devon", "Helen & William", "Vera & Dexter", "Tonya & Derrick"],
+	answer: 3
+},
+}];
+
+$('#startBtn').on('click', function(){
+	$(this).hide();
+	newGame();
+});
+
+$('#startOverBtn').on('click', function(){
+	$(this).hide();
+	newGame();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
